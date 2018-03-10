7005fa51da33772dbda7d90a15a3d46a3cdf3f4d
David Sichinava <david@sichinava.ge>
2018-03-11 00:30:02 +0400

    edit missing link

---
+++ meetings/m1/class/class1.R
@@ -0,0 +1,10 @@
+setwd("D:\\Dropbox\\My projects\\Courses\\ST_R\\website\\meetings\\m1\\class")
+getwd()
+
+# აინსტალირებს ბიბლიოთეკას
+# install.packages("ggplot2")
+library(ggplot2)
+
+# ეს კოდი უშვებს სკრიპტის ფაილს:
+source("class1.R")
+
