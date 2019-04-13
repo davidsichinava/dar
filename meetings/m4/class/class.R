5a242dfda65c91102e7becfa9ba4f1615c14c2bd
David Sichinava <david@sichinava.ge>
2019-04-13 07:20:20 +0400

    update topics page

--- meetings/m4/class/class.R
+++ meetings/m4/class/class.R
@@ -1,3 +1,4 @@
 setwd("D:\\Dropbox\\My projects\\Courses\\ST_R\\website\\meetings\\m4\\class")
 resume <- read.csv("resume.csv")
 prop.table(table(resume$call, resume$race), 2)
+0.06447639 - 0.09650924
